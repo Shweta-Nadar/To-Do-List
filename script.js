d715f92bdc9f48c88ba1f4d54dbda0a396114128
Shweta Nadar <shwetanadar@gmail.com>
2025-04-10 21:09:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 const input = document.querySelector(".input_box");
 const list = document.querySelector(".scroll");
-const todoBox = document.querySelector(".todolist_rightside");
-const counter = document.querySelector(".counter");
-
-// Function to adjust box height dynamically
-function adjustHeight() {
-    let numTasks = list.children.length;
-    todoBox.style.height = numTasks === 0 ? "100px" : `${list.scrollHeight + 50}px`;
-    counter.innerText = `Number of tasks remaining: ${numTasks}`;
-}
-
-// Function to add tasks
 function addvalue() {
     if (input.value.trim() === "") {
         alert("Kindly enter any task!");
@@ -18,7 +7,7 @@ function addvalue() {
     }
 
     let li = document.createElement("li");
-    li.textContent = input.value;
+    li.innerHTML = input.value;
 
     let span = document.createElement("span");
     span.innerHTML = "\u00d7";
@@ -28,10 +17,8 @@ function addvalue() {
     input.value = "";
 
     savedata();
-    adjustHeight();
-}
 
-// Task complete and delete event
+}
 list.addEventListener("click", function (e) {
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
@@ -39,11 +26,9 @@ list.addEventListener("click", function (e) {
     } else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
         savedata();
-        adjustHeight();
     }
 });
 
-// Save data in localStorage
 function savedata() {
     let tasks = [];
     document.querySelectorAll("li").forEach(item => {
@@ -52,8 +37,6 @@ function savedata() {
 
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
-
-// Load data when page opens
 function getdata() {
     let storedTasks = JSON.parse(localStorage.getItem("tasks"));
 
@@ -61,7 +44,7 @@ function getdata() {
         list.innerHTML = "";
         storedTasks.forEach(task => {
             let li = document.createElement("li");
-            li.textContent = task.text;
+            li.innerHTML = task.text;
 
             if (task.checked) {
                 li.classList.add("checked");
@@ -73,11 +56,8 @@ function getdata() {
             list.appendChild(li);
         });
     }
-
-    adjustHeight();
 }
 
-// Ensure data is loaded when the page starts
 document.addEventListener("DOMContentLoaded", function () {
     getdata();
 });
